39cc8f2a7e18c6720b555f3b9ffec675799a067f
AyushKadbe <ayushkadbe24@gmail.com>
2022-05-22 10:57:26 +0530

    js

--- singup.js
+++ singup.js
@@ -1,10 +1,19 @@
 function validateform(){  
    
    var password = document.forms["singup"]["password"]; 
+   var email = document.forms["singup"]["email"];
      
   
-   if(password.length<6){  
+     if(password.length<6){  
      alert("Password must be at least 6 characters long.");  
      return false;  
-     }  
+    }  
+
+    if (email.value == "") {
+      window.alert(
+        "Please enter a valid e-mail address.");
+      email.focus();
+      return false;
+    }
+    
    }
